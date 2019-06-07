a06fddef742a2513687277a8db03b95b175248cf
Clarence Ji <clare.dev@clarenceji.net>
2019-06-07 01:35:08 +0100

    Initial Commit

---
+++ Tube Status/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Tube Status
+//
+//  Created by Clarence Ji on 6/7/19.
+//  Copyright © 2019 Clarence Ji. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
